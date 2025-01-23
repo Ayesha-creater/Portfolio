483075beb1c2ff22cce8a96ee5147458c3b36eee
Zohaib Khan <dev.zohaib.work@gmail.com>
2025-01-23 20:46:44 +0500

    use netlify function instead of backend

--- backend/routes/contact.route.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const router=express.Router();
-const controller = require('../controller/contact.controller');
-
-router.post("/",controller.store)
-
-
-module.exports = router;
